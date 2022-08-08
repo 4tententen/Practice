5a84ec29ecf774cddaefda123e873d498a21b655
4tententen <4tententen@gmail.com>
2022-08-08 23:49:09 +0200

    new file 013

---
+++ python/013-exceptions.py
@@ -0,0 +1,15 @@
+#####  exceptions - wyjatki  #####
+
+country = input("Podaj nazwe kraju: ")
+
+countries_and_capitals = {"Poland": "Warsaw", "Czechia": "Prague", "Germany": "Berlin"}
+
+try:
+    print(countries_and_capitals)
+except KeyError:
+    print("Niema klucza: ")
+
+
+
+
+
